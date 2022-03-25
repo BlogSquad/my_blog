557212183721cc192edb5e0cbaeee9d7371b3671
monkeyDugi <lbd4946@gmail.com>
2022-03-25 11:18:21 +0900

    first commit

---
+++ src/main/java/project/myblog/Application.java
@@ -0,0 +1,13 @@
+package project.myblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/project/myblog/ApplicationTests.java
@@ -0,0 +1,13 @@
+package project.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
