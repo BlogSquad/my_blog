7e0bae43869b16543b529e0ae3fe9825bef81693
monkeyDugi <lbd4946@gmail.com>
2022-03-26 11:31:20 +0900

    etc: DatabaseCleanup 셋팅

---
+++ src/test/java/project/myblog/AcceptanceTest.java
@@ -0,0 +1,23 @@
+package project.myblog;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import project.myblog.utils.DatabaseCleanup;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AcceptanceTest {
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = port;
+        databaseCleanup.execute();
+    }
+}
