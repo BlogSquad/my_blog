0fc10fe3b1f5c15adb9c88469bee80f568c1ceb9
monkeyDugi <lbd4946@gmail.com>
2022-04-03 18:12:07 +0900

    etc: 패키지 위치 변경

--- src/main/java/project/myblog/exception/ErrorController.java
+++ src/main/java/project/myblog/exception/ErrorController.java
@@ -1,4 +1,4 @@
-package project.myblog.web;
+package project.myblog.exception;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
