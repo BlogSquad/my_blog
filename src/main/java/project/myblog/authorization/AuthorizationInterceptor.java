2e9c54ea3ea886b3bbbb8c832b9e58169c44e07e
monkeyDugi <lbd4946@gmail.com>
2022-04-09 20:09:03 +0900

    refactor: 인가시 예외 처리를 forward -> throw로 변경
    request에 대한 컨트롤러 메서드가 존재하면, throw를 해도 @ExceptionHandler가 핸들링이 가능하지만,
    존재하지 않으면, throw를 해도 @ExceptionHandler가 핸들링하지 못한다.

--- src/main/java/project/myblog/authorization/AuthorizationInterceptor.java
+++ src/main/java/project/myblog/authorization/AuthorizationInterceptor.java
@@ -3,26 +3,14 @@ package project.myblog.authorization;
 import org.springframework.web.servlet.HandlerInterceptor;
 import project.myblog.exception.AuthorizationException;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class AuthorizationInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         if (request.getSession(false) == null) {
-            request.setAttribute("message", "로그인이 필요합니다.");
-            request.setAttribute("exception", AuthorizationException.class);
-            try {
-                request.getRequestDispatcher("/api/error").forward(request, response);
-            } catch (ServletException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return false;
+            throw new AuthorizationException("로그인이 필요합니다.");
         }
 
         return true;
