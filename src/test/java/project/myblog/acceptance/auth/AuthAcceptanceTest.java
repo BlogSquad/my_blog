5dd0cec6c95c070b3fdcfcf1f6ea8a09030126f4
monkeyDugi <lbd4946@gmail.com>
2022-04-12 11:49:32 +0900

    refactor: 회원 정보 관리 인수테스트 파일 변경
    인증 인수 테스트에 있던 내용을 회원 정보 관리 인수 테스트로 이동

--- src/test/java/project/myblog/acceptance/auth/AuthAcceptanceTest.java
+++ src/test/java/project/myblog/acceptance/auth/AuthAcceptanceTest.java
@@ -6,14 +6,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import project.myblog.acceptance.AcceptanceTest;
 
-import static project.myblog.acceptance.auth.AuthSteps.내_회원_정보_조회_안됨;
-import static project.myblog.acceptance.auth.AuthSteps.내_회원_정보_조회_요청;
-import static project.myblog.acceptance.auth.AuthSteps.내_회원_정보_조회됨;
-import static project.myblog.acceptance.auth.AuthSteps.로그아웃_요청;
-import static project.myblog.acceptance.auth.AuthSteps.로그인_됨;
 import static project.myblog.acceptance.auth.AuthSteps.로그인_안됨;
 import static project.myblog.acceptance.auth.AuthSteps.로그인_요청;
-import static project.myblog.config.TestWebConfig.TestAbstractOAuthSessionAuthentication.AUTHORIZATION_CODE;
 import static project.myblog.auth.dto.SocialType.GITHUB;
 import static project.myblog.auth.dto.SocialType.NAVER;
 
@@ -39,54 +33,5 @@ class AuthAcceptanceTest extends AcceptanceTest {
         ExtractableResponse<Response> response = 로그인_요청("notExistsAuthorizationCode", GITHUB.getServiceName());
 
         로그인_안됨(response);
-    }
-
-    /**
-     * When 네이버 로그인 요청
-     * Then 로그인 안됨
-     * When 내 회원 정보 요청
-     * Then 회원 정보 조회됨
-     * When 로그아웃 요청
-     * When 로그인 없이 내 회원 정보 요청
-     * Then 회원 정보 조회 안됨
-     */
-    @Test
-    void 네이버_세션_내_정보_조회_관리() {
-        ExtractableResponse<Response> loginResponse = 로그인_요청(AUTHORIZATION_CODE, NAVER.getServiceName());
-        String sessionId = loginResponse.sessionId();
-        로그인_됨(loginResponse);
-
-        ExtractableResponse<Response> membersMeResponse = 내_회원_정보_조회_요청(sessionId);
-        내_회원_정보_조회됨(membersMeResponse);
-
-        로그아웃_요청(sessionId);
-
-        ExtractableResponse<Response> response = 내_회원_정보_조회_요청(sessionId);
-        내_회원_정보_조회_안됨(response);
-    }
-
-    /**
-     * When 깃허브 로그인 요청
-     * Then 로그인 안됨
-     * When 내 회원 정보 요청
-     * Then 회원 정보 조회됨
-     * When 로그아웃 요청
-     * When 로그인 없이 내 회원 정보 요청
-     * Then 회원 정보 조회 안됨
-     */
-    @Test
-    void 깃허브_세션_내_정보_조회_관리() {
-        ExtractableResponse<Response> loginResponse = 로그인_요청(AUTHORIZATION_CODE, GITHUB.getServiceName());
-
-        String sessionId = loginResponse.sessionId();
-        로그인_됨(loginResponse);
-
-        ExtractableResponse<Response> membersMeResponse = 내_회원_정보_조회_요청(sessionId);
-        내_회원_정보_조회됨(membersMeResponse);
-
-        로그아웃_요청(sessionId);
-
-        ExtractableResponse<Response> response = 내_회원_정보_조회_요청(sessionId);
-        내_회원_정보_조회_안됨(response);
     }
 }
--- src/test/java/project/myblog/acceptance/auth/AuthSteps.java
+++ src/test/java/project/myblog/acceptance/auth/AuthSteps.java
@@ -4,7 +4,6 @@ import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static project.myblog.config.WebConfig.SESSION_LOGIN_URI;
@@ -34,24 +33,6 @@ public class AuthSteps {
 
     public static void 로그인_안됨(ExtractableResponse<Response> response) {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
-        assertThat(response.sessionId()).isNullOrEmpty();
-    }
-
-    public static ExtractableResponse<Response> 내_회원_정보_조회_요청(String sessionId) {
-        return RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .cookie("JSESSIONID", sessionId)
-                .when().get("members/me")
-                .then().log().all().extract();
-    }
-
-    public static void 내_회원_정보_조회됨(ExtractableResponse<Response> response) {
-        assertThat(response.jsonPath().getString("email")).isEqualTo("monkeyDugi@gmail.com");
-    }
-
-    public static void 내_회원_정보_조회_안됨(ExtractableResponse<Response> response) {
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.FORBIDDEN.value());
         assertThat(response.sessionId()).isNullOrEmpty();
     }
 }
