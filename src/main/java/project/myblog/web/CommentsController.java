109c3d54ac5ea178ac28c2e705a1338b6134dfe2
monkeyDugi <lbd4946@gmail.com>
2022-04-21 15:49:27 +0900

    feat: Controller에 Service 적용

--- src/main/java/project/myblog/web/CommentsController.java
+++ src/main/java/project/myblog/web/CommentsController.java
@@ -2,17 +2,30 @@ package project.myblog.web;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import project.myblog.auth.dto.Login;
 import project.myblog.auth.dto.LoginMember;
+import project.myblog.service.CommentsService;
+import project.myblog.web.dto.comments.CommentsRequest;
 
+import javax.validation.Valid;
 import java.net.URI;
 
 @RestController
 public class CommentsController {
+    private final CommentsService commentsService;
+
+    public CommentsController(CommentsService commentsService) {
+        this.commentsService = commentsService;
+    }
+
     @PostMapping(value = "/posts/{id}/comments", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Void> createComments(@Login LoginMember loginMember) {
-        return ResponseEntity.created(URI.create("/posts/1/comments")).build();
+    public ResponseEntity<Void> createComments(@Login LoginMember loginMember, @PathVariable Long id,
+                                               @Valid @RequestBody CommentsRequest commentsRequest) {
+        Long commentsId = commentsService.createComments(loginMember.getEmail(), id, commentsRequest);
+        return ResponseEntity.created(URI.create("/posts/" + commentsId + "/comments")).build();
     }
 }
