d2daaa21259508dc78850ff1874c5780473050aa
monkeyDugi <lbd4946@gmail.com>
2022-05-13 18:16:17 +0900

    test: 네이밍 변경 및 CountDownLatch 적용

--- src/test/java/project/myblog/utils/RedisTest.java
+++ src/test/java/project/myblog/utils/RedisTest.java
@@ -11,6 +11,10 @@ import project.myblog.repository.HitsRedisRepository;
 import project.myblog.repository.MemberRepository;
 import project.myblog.repository.PostRepository;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static project.myblog.acceptance.member.MemberStepsRequest.NAVER_EMAIL;
 
@@ -36,47 +40,30 @@ public class RedisTest {
         Long postId = 1L;
 
         // when
-        hitsRedisRepository.increaseHits(postId);
-        hitsRedisRepository.increaseHits(postId);
+        hitsRedisRepository.incrementHits(postId);
+        hitsRedisRepository.incrementHits(postId);
 
         // then
         assertThat(hitsRedisRepository.getHits(postId)).isEqualTo(2);
     }
 
-    @Transactional
     @Test
-    void 조회수_RDB에_반영() {
-        // given
-        Member member = new Member(NAVER_EMAIL);
-        memberRepository.save(member);
-
-        Post post = new Post("포스트1제목", "포스트1내용", member);
-        Long postId = postRepository.save(post).getId();
-
-        hitsRedisRepository.increaseHits(postId);
-        hitsRedisRepository.increaseHits(postId);
-
-        // when
-        hitsRedisRepository.updateRDB();
-
-        // then
-        assertThat(hitsRedisRepository.getHits(postId)).isNull();
-
-        Post findPost = postRepository.findById(postId).get();
-        assertThat(findPost.getHits()).isEqualTo(2);
-    }
-
-    @Test
-    void 조회수_증가_동시성_테스트() {
-        for (int i = 1; i <= 10_000; i++) {
-            Runnable run = () -> hitsRedisRepository.increaseHits(1L);
-            new Thread(run).start();
+    void 조회수_증가_동시성_테스트() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        CountDownLatch countDownLatch = new CountDownLatch(10);
+        for (int i = 1; i <= 10; i++) {
+            executorService.execute(() -> {
+                hitsRedisRepository.incrementHits(1L);
+                countDownLatch.countDown();
+            });
         }
 
+        countDownLatch.await();
         Integer hits = hitsRedisRepository.getHits(1L);
-        assertThat(hits).isEqualTo(10_000);
+        assertThat(hits).isEqualTo(10);
     }
 
+    @Transactional
     @Test
     void 조회수_RDB_반영() {
         // given
@@ -86,7 +73,7 @@ public class RedisTest {
         Post post = new Post("포스트1제목", "포스트1내용", member);
         Long postId = postRepository.save(post).getId();
 
-        hitsRedisRepository.increaseHits(postId);
+        hitsRedisRepository.incrementHits(postId);
 
         // when
         hitsRedisRepository.updateRDB();
@@ -94,10 +81,5 @@ public class RedisTest {
         // then
         assertThat(hitsRedisRepository.getHits(postId)).isNull();
         assertThat(postRepository.findById(postId).get().getHits()).isEqualTo(1);
-    }
-
-    @Test
-    void 조회수_RDB반영_동시성_테스트() {
-
     }
 }
