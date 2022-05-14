912c7acd888e4082ab559e13cbe6485947bd05f2
monkeyDugi <lbd4946@gmail.com>
2022-05-14 18:13:24 +0900

    feat: 포스트 목록 조회 서비스 테스트 작성

---
+++ src/test/java/project/myblog/unit/UnitTest.java
@@ -0,0 +1,27 @@
+package project.myblog.unit;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.Transactional;
+import project.myblog.config.TestWebConfig;
+import project.myblog.repository.HitsRepository;
+import project.myblog.utils.DatabaseCleanup;
+
+@Transactional
+@Import(TestWebConfig.class)
+@SpringBootTest
+public class UnitTest {
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+
+    @Autowired
+    private HitsRepository hitsRepository;
+
+    @BeforeEach
+    public void setUp() {
+        databaseCleanup.execute();
+        hitsRepository.flushAll();
+    }
+}
