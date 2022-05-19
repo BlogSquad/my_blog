8b16e2652007e3fdbcbd1103023c15aaaf6f9ad5
monkeyDugi <53487385+monkeyDugi@users.noreply.github.com>
2022-05-19 21:44:00 +0900

    readme 정리 (#82)
    * etc: 설정 정보 변경
    
    - 내장 h2로 변경
    - 실환경 설정 추가
    - 자바 11로 변경 Optional.isEmpty() 사용
    
    * etc: 운영 디비 설정
    
    * etc: 운영과 로컬간 리다이렉트 주소가 다르기 때문에 설정 파일로 변경
    
    * etc: 운영서버 파라미터 타입 info로 변경
    
    * docs: readme 작성
    
    * etc: 운영 디비 설정 추가

--- src/main/java/project/myblog/auth/dto/SocialType.java
+++ src/main/java/project/myblog/auth/dto/SocialType.java
@@ -4,28 +4,24 @@ public enum SocialType {
     NAVER(
             "naver",
             "https://nid.naver.com/oauth2.0/token",
-            "",
             "authorization_code",
             "https://openapi.naver.com/v1/nid/me"
     ),
     GITHUB(
             "github",
             "https://github.com/login/oauth/access_token",
-            "http://localhost:8080/login/oauth2/code/github",
             "",
             "https://api.github.com/user/emails"
     );
 
     private final String serviceName;
     private final String accessTokenUri;
-    private final String redirectUri;
     private final String grantType;
     private final String apiMeUri;
 
-    SocialType(String serviceName, String accessTokenUri, String redirectUri, String grantType, String apiMeUri) {
+    SocialType(String serviceName, String accessTokenUri, String grantType, String apiMeUri) {
         this.serviceName = serviceName;
         this.accessTokenUri = accessTokenUri;
-        this.redirectUri = redirectUri;
         this.grantType = grantType;
         this.apiMeUri = apiMeUri;
     }
@@ -36,10 +32,6 @@ public enum SocialType {
 
     public String getAccessTokenUri() {
         return accessTokenUri;
-    }
-
-    public String getRedirectUri() {
-        return redirectUri;
     }
 
     public String getGrantType() {
