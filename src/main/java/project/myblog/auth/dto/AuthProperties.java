c2717263481d729d502c690ad8820e6388d63138
monkeyDugi <lbd4946@gmail.com>
2022-05-25 19:06:15 +0900

    refactor: github oauth 요청 redirect url 제거
    redirect url이 없으면 자동으로 call back url로 매핑되기 때문에 제거

--- src/main/java/project/myblog/auth/dto/AuthProperties.java
+++ src/main/java/project/myblog/auth/dto/AuthProperties.java
@@ -15,9 +15,6 @@ public class AuthProperties {
     @Value("${oauth.github.client_secret_id}")
     private String githubClientSecret;
 
-    @Value("${oauth.github.redirectUri}")
-    private String githubRedirectUri;
-
     public String getNaverClientId() {
         return naverClientId;
     }
@@ -33,8 +30,5 @@ public class AuthProperties {
     public String getGithubClientSecret() {
         return githubClientSecret;
     }
-
-    public String getGithubRedirectUri() {
-        return githubRedirectUri;
-    }
 }
+
